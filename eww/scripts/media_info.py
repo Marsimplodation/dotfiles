163c95f05b0e26e1f2dfdbac9de72208bfaddc5d
Marius Werkmeister <m.werkmeister@tu-braunschweig.de>
2025-10-26 21:37:14 +0100

    Update dotfiles (So 26. Okt 21:37:14 CET 2025)

--- eww/scripts/media_info.py
+++ eww/scripts/media_info.py
@@ -1,10 +1,22 @@
 import dbus
 import json
+import os
+from PIL import Image, ImageFilter
 bus = dbus.SessionBus()
 
 
 
 no = False;
+
+def blur_img(pic_path):
+    if not os.path.exists(pic_path):
+        return pic_path  # fallback if file missing
+    im = Image.open(pic_path)
+    output_path = "/tmp/cover_blur.png"
+    blurred = im.filter(ImageFilter.GaussianBlur(10))
+    blurred.save(output_path)
+    return output_path
+
 for service in bus.list_names():
     if service.startswith('org.mpris.MediaPlayer2.'):
         player = dbus.SessionBus().get_object(service, '/org/mpris/MediaPlayer2')
@@ -18,8 +30,8 @@ for service in bus.list_names():
         if not pic == None:
             pic = pic.replace("dbus.String(", "").split(",")[0]
             pic = pic.replace("file://", "")
-        print(json.dumps({"Playing": (status == "Playing"), "title": title, "artist": artist, "pic": pic}))
+        print(json.dumps({"Playing": (status == "Playing"), "title": title, "artist": artist, "cover": pic, "cover_blured": blur_img(pic)}))
         no=True
     
 if not no:
-    print(json.dumps({"Playing": False, "title": "none", "artist": "none", "pic": ""}))
+    print(json.dumps({"Playing": False, "title": "none", "artist": "none", "cover": "", "cover_blurred": ""}))
